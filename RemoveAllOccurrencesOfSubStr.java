c1442536b46d4f0374400b60cf1ce408b50e9596
iammadhu123 <myadav07012006@gmail.com>
2026-03-14 22:28:35 +0530

    solved

---
+++ RemoveAllOccurrencesOfSubStr.java
@@ -0,0 +1,53 @@
+public class RemoveAllOccurrencesOfSubStr {
+    // public static String removeOccurrences(String s, String part) {
+    //     while (s.contains(part)) {
+    //         s = s.replace(part, "");
+    //     }
+    //     return s;
+    // } // Failed Test Case s = "aabababa", part = "aba" → Output: "ab" Expected: "ba"
+
+    // public static String removeOccurrences(String s, String part) {
+    //     StringBuilder sb = new StringBuilder(s);
+
+    //     while (sb.indexOf(part) != -1) { //indexOf() is a method used to find the position of a substring or character inside a string.
+    //         int idx = sb.indexOf(part);
+    //         sb.delete(idx, idx + part.length());
+    //     }
+
+    //     return sb.toString();
+    // }
+    // public static String removeOccurrences(String s, String part) {
+    //     int index = s.indexOf(part);
+    //     while (index != -1) {
+    //         s = s.substring(0, index) + s.substring(index + part.length());
+    //         index = s.indexOf(part);
+    //     }
+    //     return s;
+    // }
+
+    public static String removeOccurrences(String s, String part) {
+        StringBuilder stack = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            stack.append(c);
+            if (stack.length() >= part.length()) {
+                int start = stack.length() - part.length();
+                if (stack.substring(start).equals(part)) {
+                    stack.delete(start, stack.length());
+                }
+            }
+        }
+        return stack.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "aabababa", part = "aba";//"daabcbaabcbc";
+        // String part = "abc";
+        System.out.println(removeOccurrences(s, part));
+    }
+}
+
+// Think of StringBuilder as a stack:
+
+// push character
+// check last k letters
+// if equal → pop them
--- reverseWordsInStr.java
+++ reverseWordsInStr.java
@@ -16,9 +16,9 @@ public class reverseWordsInStr {
     // }
 
     public static String reverseWords(String s) {// TC = O(n) SC = O(1)
-        s = s.trim().replaceAll("\\s+", " ");
-        StringBuilder sb = new StringBuilder(s);
-        sb.reverse();
+        s = s.trim().replaceAll("\\s+", " "); //trim() → removes extra spaces from start and end of the string, replaceAll("\\s+", " ") → replaces multiple spaces with a single space //s+ → means one or more spaces //replaceAll("\\s+", " ") → converts multiple spaces into one space
+        StringBuilder sb = new StringBuilder(s); // Convert string to StringBuilder for in-place modification
+        sb.reverse(); //Reversed:  eulb si yks eht
 
         int start = 0;
         for(int end = 0; end < sb.length(); end++) {
@@ -46,6 +46,15 @@ public class reverseWordsInStr {
         System.out.println(reverseWords(s));
     }
 }
+
+// reverse(sb, start, sb.length() - 1);
+// 2️⃣ Why this line is needed
+// Inside the loop we reverse words when we see a space:
+// if (sb.charAt(end) == ' ')
+// But the last word has no space after it.
+// So it never gets reversed inside the loop.
+// Therefore we manually reverse it.
+
 
 
 // class Solution { // TC = O(n) SC = O(n)
