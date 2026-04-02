5b7c0a53a40d514346c267fd92248c9f5cb7b74d
iammadhu123 <myadav07012006@gmail.com>
2026-04-02 22:52:58 +0530

    solved

---
+++ GroupAnagrams.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class GroupAnagrams  {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
