dc34b06830f0496c0f928740389b9b422b8d2726
Madhu Yadav <myadav07012006@gmail.com>
2026-08-18 10:57:52 +0530

    Time: 0 ms (100%), Space: 46.5 MB (42.89%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.java
@@ -14,35 +14,61 @@
  * }
  */
 class Solution {
-    public int findBottomLeftValue(TreeNode root) {
+    int maxDepth = -1;
+    int ans;
 
-        Queue<TreeNode> q = new LinkedList<>();
-
-        //insert the root node
-        q.offer(root);
-
-        int ans = root.val;
-
-        while(!q.isEmpty()) {
-            int size = q.size();
-
-            for(int i = 0; i<size; i++) {
-                TreeNode node = q.poll();
-
-                // last level ka first node
-                if (i == 0) {
-                    ans = node.val;
-                }
-
-                if(node.left != null) {
-                    q.offer(node.left);
-                }
-                if(node.right != null) {
-                    q.offer(node.right);
-                }
-            }
+    public void dfs(TreeNode node, int depth) {
+        if(node == null) {
+            return;
         }
+        if (depth > maxDepth) {
+            maxDepth = depth;
+            ans = node.val;
+        }
+        //left first
+        dfs(node.left, depth + 1);
+        //right 2nd
+        dfs(node.right, depth + 1);
+    }
+
+    public int findBottomLeftValue(TreeNode root) {
+        //DFS Approach
+        dfs(root, 0);
 
         return ans;
+
+        //BFS Approach
+
+        // Queue<TreeNode> q = new LinkedList<>();
+
+        // //insert the root node
+        // q.offer(root);
+
+        // int ans = root.val;
+
+        // while(!q.isEmpty()) {
+        //     int size = q.size();
+
+        //     // // current level ka first node
+        //     // ans = q.peek().val;  //OR
+
+        //     for(int i = 0; i<size; i++) {
+        //         TreeNode node = q.poll();
+
+        //         // last level ka first node
+        //         if (i == 0) {
+        //             ans = node.val;
+        //         }
+
+        //         if(node.left != null) {
+        //             q.offer(node.left);
+        //         }
+        //         if(node.right != null) {
+        //             q.offer(node.right);
+        //         }
+        //     }
+        // }
+
+        // return ans;
     }
 }
