5abd90275dc72477d482842b092199f68f003971
Madhu Yadav <myadav07012006@gmail.com>
2026-08-19 11:15:01 +0530

    Time: 7 ms (45.53%), Space: 42 MB (70.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0) {
-            return 0;
+        if(n == 0 || n == 1) {
+            return n;
         }
-        if(n <= 2) {
+        if(n == 2) {
             return 1;
         }
 
